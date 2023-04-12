36116c9af5be356896f122db0adffca79921e4a5
Gee T <gee.toca@outlook.com>
2023-04-12 08:16:43 +0800

    Initial commit

---
+++ catnapping.py
@@ -0,0 +1,15 @@
+#Multiline Strings with Triple Quotes
+
+print('''Dear Alice,
+
+Eve;s cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+Bob''')
+
+"""This is a test Python program.
+
+Written by Gee Toca
+
+This program was designed for Python 3, not Python 2.
+"""
---
+++ validateInput.py
@@ -0,0 +1,14 @@
+while True:
+    print('Enter your age:')
+    age = input()
+    if age.isdecimal():
+        break
+    print('\nPlease enter a number for your age.\n')
+
+while True:
+    print('Select a new passsword (letters and numbers only):')
+    password = input()
+    if password.isalnum():
+        break
+    print('\nPasswords can only have letters and numbers.\n')
+
